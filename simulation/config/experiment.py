80d79f43f450402a90015fa9f5e22c5e744be086
SeanMcOwen <seanmcowen@gmail.com>
2024-10-23 21:20:52 -0400

    New experiments

--- simulation/config/experiment.py
+++ simulation/config/experiment.py
@@ -19,3 +19,25 @@ experiments_map["Baseline"] = {
     "Blocks": blocks,
     "Monte Carlo Runs": 5,
 }
+
+experiments_map["Control Option V2 Low D Probability"] = {
+    "Name": "Control Option V2 Low D Probability",
+    "Param Modifications": {
+        "FP DUMMY Length-1 DEF Control Action": "DUMMY Length-1 DEF D Probability Option",
+        "DUMMY D Probability": 0.1,
+    },
+    "State Modifications": None,
+    "Blocks": blocks,
+    "Monte Carlo Runs": 5,
+}
+
+experiments_map["Control Option V2 High D Probability"] = {
+    "Name": "Control Option V2 High D Probability",
+    "Param Modifications": {
+        "FP DUMMY Length-1 DEF Control Action": "DUMMY Length-1 DEF D Probability Option",
+        "DUMMY D Probability": 0.9,
+    },
+    "State Modifications": None,
+    "Blocks": blocks,
+    "Monte Carlo Runs": 5,
+}
