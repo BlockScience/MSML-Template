4e78e7b62ee07ff7f40fcbbca1617e467c7a0846
SeanMcOwen <seanmcowen@gmail.com>
2024-10-29 10:16:14 -0400

    Add metaprogramming notebook

---
+++ model/boundary_actions.py
@@ -0,0 +1,9 @@
+from .spaces import EmptySpace, DUMMYABCDEFSpace
+
+
+def dummy_length_1_abc_boundary_action(state, params) -> DUMMYABCDEFSpace:
+    pass
+
+
+def dummy_length_2_abc_combo_boundary_action(state, params) -> DUMMYABCDEFSpace:
+    pass
