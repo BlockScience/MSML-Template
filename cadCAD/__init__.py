ebf221b8591df2d9da47eac4e93e5dfacea66a38
SeanMcOwen <seanmcowen@gmail.com>
2025-01-21 15:42:52 -0500

    cadCAD model

---
+++ cadCAD/__init__.py
@@ -0,0 +1,3 @@
+from .models1 import model1, model2
+
+cadCAD_models = {"Model 1": model1, "Model 2": model2}
--- cadCAD/models1.py
+++ cadCAD/models1.py
@@ -1,5 +1,35 @@
 import os
 import sys
+from copy import deepcopy
 
 sys.path.append(os.path.abspath("../.."))
 from MSML.src.math_spec_mapping import load_from_json
+
+
+from src import math_spec_json
+from simulation import compute_starting_total_length, check_d_probability
+
+ms = load_from_json(deepcopy(math_spec_json))
+
+blocks1 = [
+    "DUMMY Length-2 Boundary Wiring",
+    "DUMMY Length-1 Boundary Wiring",
+    "DUMMY Control Wiring",
+]
+
+model1 = ms.build_cadCAD(
+    blocks1,
+    state_preperation_functions=[compute_starting_total_length],
+    parameter_preperation_functions=[check_d_probability],
+)
+
+blocks2 = [
+    "DUMMY Length-2 Boundary Wiring",
+    "DUMMY Length-1 Boundary Wiring",
+]
+
+model2 = ms.build_cadCAD(
+    blocks2,
+    state_preperation_functions=[compute_starting_total_length],
+    parameter_preperation_functions=[check_d_probability],
+)
